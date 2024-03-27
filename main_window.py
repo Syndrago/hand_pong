8193c199378b28915c5cc2898eb122af85b77596
SyndragoAlpenglow <synclair@alpenglowbiosciences.com>
2024-03-27 11:01:59 -0700

    got all components

--- main_window.py
+++ main_window.py
@@ -1,4 +1,6 @@
 import pygame
+import cv2 # Import the OpenCV library
+import numpy as np # Import Numpy library
 
 pygame.init()
 
@@ -113,10 +115,103 @@ class Ball:
 
 	def getRect(self):
 		return self.ball
+	
+class Webcam:
+
+	def __init__(self):
+    # Create a VideoCapture object
+		self.cap = cv2.VideoCapture(0)
+	
+		# Create the background subtractor object
+		# Use the last 700 video frames to build the background
+		self.back_sub = cv2.createBackgroundSubtractorMOG2(history=700, 
+			varThreshold=25, detectShadows=True)
+	
+		# Create kernel for morphological operation
+		# You can tweak the dimensions of the kernel
+		# e.g. instead of 20,20 you can try 30,30.
+		self.kernel = np.ones((20,20),np.uint8)
+		self.im_frame = 0
+
+	def cap_images(self):
+ 
+		while(True):
+	
+			# Capture frame-by-frame
+			# This method returns True/False as well
+			# as the video frame.
+			ret, frame = self.cap.read()
+	
+			# Use every frame to calculate the foreground mask and update
+			# the background
+			fg_mask = self.back_sub.apply(frame)
+	
+			# Close dark gaps in foreground object using closing
+			fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, self.kernel)
+	
+			# Remove salt and pepper noise with a median filter
+			fg_mask = cv2.medianBlur(fg_mask, 5) 
+			
+			# Threshold the image to make it either black or white
+			_, fg_mask = cv2.threshold(fg_mask,127,255,cv2.THRESH_BINARY)
+	
+			# Find the index of the largest contour and draw bounding box
+			fg_mask_bb = fg_mask
+			contours, hierarchy = cv2.findContours(fg_mask_bb,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+			areas = [cv2.contourArea(c) for c in contours]
+	
+			# If there are no countours
+			if len(areas) < 1:
+	
+				# Display the resulting frame
+				# cv2.imshow('frame',frame)
+	
+				# If "q" is pressed on the keyboard, 
+				# exit this loop
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+	
+				# Go to the top of the while loop
+				self.im_frame = frame
+				continue
+	
+			else:
+				# Find the largest moving object in the image
+				max_index = np.argmax(areas)
+	
+			# Draw the bounding box
+			cnt = contours[max_index]
+			x,y,w,h = cv2.boundingRect(cnt)
+			cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+	
+			# Draw circle in the center of the bounding box
+			x2 = x + int(w/2)
+			y2 = y + int(h/2)
+			cv2.circle(frame,(x2,y2),4,(0,255,0),-1)
+	
+			# Print the centroid coordinates (we'll use the center of the
+			# bounding box) on the image
+			text = "x: " + str(x2) + ", y: " + str(y2)
+			cv2.putText(frame, text, (x2 - 10, y2 - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+			
+			# Display the resulting frame
+			# cv2.imshow('frame',frame)
+			self.im_frame = frame
+
+	def cam_stop(self):
+		# If "q" is pressed on the keyboard, 
+		# exit this loop
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			# break
+			pass
+ 
+		# Close down the video stream
+		self.cap.release()
+		cv2.destroyAllWindows()
+
 
 # Game Manager
-
-
 def main():
 	running = True
 
@@ -124,6 +219,8 @@ def main():
 	# geek1 = Striker(20, 0, 10, 100, 10, GREEN)
 	geek2 = Striker(WIDTH-30, 0, 10, 100, 10, GREEN)
 	ball = Ball(WIDTH//2, HEIGHT//2, 7, 7, WHITE)
+	# webcam = Webcam()
+	# webcam.cap_images()
 
 	# listOfGeeks = [geek1, geek2]
 	listOfGeeks = [geek2]
@@ -185,6 +282,9 @@ def main():
 						geek2Score, WIDTH-100, 20, WHITE)
 
 		pygame.display.update()
+		img = pygame.image.load("background.png")
+		print(img)
+		screen.blit(img,(258,178))
 		clock.tick(FPS)	 
 
 
