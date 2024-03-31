5b8b14f427c21d11fe91256696e91abed43f51da
SyndragoUW <zhen0803@uw.edu>
2024-03-31 02:12:39 -0700

    Code commented

--- hand_pong.py
+++ hand_pong.py
@@ -209,7 +209,9 @@ class Webcam:
             camera_number (int): Camera number for capturing video.
         """
         self.cap = cv2.VideoCapture(camera_number, cv2.CAP_DSHOW)
-        self.back_sub = cv2.createBackgroundSubtractorMOG2(history=700, varThreshold=25, detectShadows=True)
+        # Background subctractor
+        self.back_sub = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold=25, detectShadows=True)
+        # Kernel for morphological operation
         self.kernel = np.ones((20, 20), np.uint8)
         self.im_frame = None
         self.max_index = 0
@@ -221,27 +223,42 @@ class Webcam:
         """
         ret, frame = self.cap.read()
 
+        # Calculate foreground mask
         fg_mask = self.back_sub.apply(frame)
+        # Remove noise using erosion
         fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, self.kernel)
+        # Smoothen noise with blur
         fg_mask = cv2.medianBlur(fg_mask, 5)
+        # Threshold the image structure
         _, fg_mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY)
-
+        # Find the index of the largest contour
         contours, _ = cv2.findContours(fg_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+        # If there is no contour
         if len(contours) < 1:
             self.im_frame = frame
             self.y_pos = self.im_frame.shape[0] // 2
+        # If there contours are found
         else:
+            # Find the index of the largest contour
             areas = [cv2.contourArea(c) for c in contours]
             self.max_index = np.argmax(areas)
+
+            # Draw the bounding box
             cnt = contours[self.max_index]
             x, y, w, h = cv2.boundingRect(cnt)
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+            # Draw circle in the center of the bounding box
             x2 = x + int(w / 2)
             y2 = y + int(h / 2)
             frame = cv2.circle(frame, (x2, y2), 4, (0, 255, 0), -1)
+
+            # Print the centroid coordinates
             text = "x: " + str(x2) + ", y: " + str(y2)
             frame = cv2.putText(frame, text, (x2 - 10, y2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            # Update the image and centroid height
             self.im_frame = frame
             self.y_pos = y2
 
@@ -258,9 +275,13 @@ def main():
     Main function to run the game.
     """
     running = True
+
+    # Initialize game objects
     paddle = Paddle(WIDTH - 30, 0, 10, 100, 10, GREEN)
     ball = Ball(WIDTH // 2, HEIGHT // 2, 7, 7, WHITE)
     webcam = Webcam(camera_number)
+
+    # Initial player score and paddle direction factor.
     player_score = 0
     player_YFac = 0
 
@@ -272,6 +293,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Set paddle direction factor based on hand position on webcam.
+        # By default for Logitech C615:
+        # Middle 50 pixels = no movement
+        # Top and bottom 75 pixels = double the speed
+        # Between 25 from middle and 75 from top or bottom pixels = normal speed.
         player_YFac = 0
         if webcam.y_pos >= webcam.im_frame.shape[0] - move_pixel_buffer*3:
             player_YFac = 2
@@ -282,6 +308,7 @@ def main():
         elif webcam.y_pos < webcam.im_frame.shape[0] // 2 - move_pixel_buffer:
             player_YFac = -1
 
+        # Collision detection between ball and paddle
         if ball.getRect().colliderect(paddle.getRect()):
             ball.hit(paddle.posx)
             pygame.mixer.music.load(bounce_abs_path)
@@ -289,18 +316,24 @@ def main():
             paddle.color = random.choice(color_list)
             player_score += 1
 
+        # Update paddle position
         paddle.update(player_YFac)
+        
+        # Update ball position and get point
         point = ball.update()
 
+        # Update player score
         if point == -1:
             if player_score > 0:
                 player_score -= 1
         elif point == 1:
             player_score += 1
 
+        # Reset ball
         if point:
             ball.reset()
 
+        # Display game objects
         paddle.display()
         ball.display()
         paddle.display_score("Score: ", player_score, WIDTH - 100, 20, WHITE)
@@ -311,10 +344,10 @@ def main():
         )
         img.set_alpha(overlay_opacity)
         screen.blit(img, img.get_rect())
-
         pygame.display.update()
         clock.tick(FPS)
 
+    # Stop the webcam and game
     webcam.cam_stop()
     pygame.quit()
 
